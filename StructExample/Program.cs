72ab22616b2872a34e4bbdb6cbe06695abd8fc1e
Stefan Sjöberg <bird.nerd@outlook.com>
2024-12-03 13:49:48 +0100

    Add project files.

---
+++ StructExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExample
+{
+    struct Order
+    {
+        public string ItemName { get; set; }
+        public int UnitCount { get; set; }
+        public double UnitCost { get; set; }
+
+        public double TotalCost()
+        {
+            double res = UnitCount * UnitCost;
+            return res;
+        }
+
+        public string WriteOrder()
+        {
+            return String.Format("Lager har {0} {1} som kostar {2} styck och total kostnad blir {3}", UnitCount, ItemName, UnitCost, TotalCost());
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order1 = new Order();
+            order1.ItemName = "poster";
+            order1.UnitCost = 154.53;
+            order1.UnitCount = 7;
+
+            Console.WriteLine(order1.WriteOrder());
+        }
+    }
+}
